1a39f12f9c5288839cb291df67a627a4c65cc5b0
Enes Kanbur <eneskanbur02@hotmail.com>
2025-10-22 22:37:14 +0300

    First commit

---
+++ Bouncing.kt
@@ -0,0 +1,23 @@
+class Bouncing {
+
+    fun bouncingBall(h: Double, bounce: Double, window: Double): Int {
+        if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) {
+            return -1
+        }
+
+        var result = 1
+        var high = h * bounce
+
+        if (h == window) {
+            return result.toInt()
+        }
+
+        while (high > window) {
+            result += 2
+            high *= bounce
+        }
+
+        return result.toInt()
+    }
+
+}
---
+++ LeapYears.kt
@@ -0,0 +1,7 @@
+class LeapYears() {
+
+    fun isLeapYear(year: Int) : Boolean {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+    }
+
+}
